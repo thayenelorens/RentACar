9123a30fa17ff512aacf8a350907ee1d87a5426d
Thayene Lorens <thayenelorens@gmail.com>
2021-11-03 12:24:30 +0000

    Create RentACar.java
    Implementation

---
+++ src/ooc/yoursolution/RentACar.java
@@ -0,0 +1,58 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ooc.yoursolution;
+
+import java.util.List;
+import ooc.enums.Make;
+import ooc.enums.Month;
+
+/**
+ *
+ * @author Thayene Lorens and Mayara Lorens
+ */
+public class RentACar implements RentACarInterface {
+
+    @Override
+    public List<CarInterface> getCars() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void setCars(List<CarInterface> cars) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public String getName() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void setName(String name) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean checkAvailability(Month month, int day, Make make, int lengthOfRent) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public int getCarAvailable(Month month, int day, Make make, int lengthOfRent) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean bookCar(Month month, int day, Make make, int lengthOfRent) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public int getNumberOfCars() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
